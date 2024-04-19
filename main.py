5989717e8b8c61d24e311512985f374fc0c0801c
Kanishkumar <kanishkumar.24cs@licet.ac.in>
2024-04-19 15:00:57 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from PIL import Image
+
+hide_menu = """
+<style>
+#MainMenu{
+    visibility:hidden;
+}
+
+footer{
+    visibility:hidden;
+}
+</style>
+"""
+
+
+
+
+
+
+
+
+
